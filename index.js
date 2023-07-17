423d7b10029962b34cb76889f467627e0498b0aa
Yogesh Yadav <59149010+yogeshft@users.noreply.github.com>
2023-07-17 06:51:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,12 @@ function handleScroll() {
 
   document.getElementById("progressBar").style.width = scrolled + "%";
 }
-// document.getElementByClassName("btnChange").addEventListener("click", changeBackground);
-// function changeBackground() {
-//     var element = document.getElementsByClassName("tabs");
 
-//     element.style.backgroundImage = "url('tab-2.png')";
-//   }
 
 const nav4Item1 = document.getElementById("nav-home-tab");
 const nav4Item2 = document.getElementById("nav-profile-tab");
 const sect4Bg = document.getElementsByClassName("accordion-body");
 
-// console.log("1.", nav4Item1);
-// console.log("2.", nav4Item2);
-// console.log("3.", sect4Bg);
 
 nav4Item1.addEventListener("click", () => {
   document.getElementsByClassName("nav-4-item")[0].classList.toggle("active");
